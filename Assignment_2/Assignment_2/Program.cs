6d6e7a3a1e3db0cf3c11fb2f9d440314f236303f
Conor Wadsworth <19746694@students.lincoln.ac.uk>
2020-03-21 14:22:58 +0000

    Added comments

--- Assignment_2/Assignment_2/Program.cs
+++ Assignment_2/Assignment_2/Program.cs
@@ -10,49 +10,49 @@ namespace Assignment_2
     {
         static void Main(string[] args)
         {
-            string[] git_1a = System.IO.File.ReadAllLines(@"GitRepositories_1a.txt");
-            string[] git_1b = System.IO.File.ReadAllLines(@"GitRepositories_1b.txt");
-            string[] git_2a = System.IO.File.ReadAllLines(@"GitRepositories_2a.txt");
-            string[] git_2b = System.IO.File.ReadAllLines(@"GitRepositories_2b.txt");
-            string[] git_3a = System.IO.File.ReadAllLines(@"GitRepositories_3a.txt");
-            string[] git_3b = System.IO.File.ReadAllLines(@"GitRepositories_3b.txt");
+            string[] git_1a = System.IO.File.ReadAllLines(@"GitRepositories_1a.txt"); //Importing the text files and putting them into a string array
+            string[] git_1b = System.IO.File.ReadAllLines(@"GitRepositories_1b.txt"); //Importing the text files and putting them into a string array
+            string[] git_2a = System.IO.File.ReadAllLines(@"GitRepositories_2a.txt"); //Importing the text files and putting them into a string array
+            string[] git_2b = System.IO.File.ReadAllLines(@"GitRepositories_2b.txt"); //Importing the text files and putting them into a string array
+            string[] git_3a = System.IO.File.ReadAllLines(@"GitRepositories_3a.txt"); //Importing the text files and putting them into a string array
+            string[] git_3b = System.IO.File.ReadAllLines(@"GitRepositories_3b.txt"); //Importing the text files and putting them into a string array
 
-            Console.WriteLine("Which set of files would you like to compare? 1,2 or 3 ");
-            string choice = Console.ReadLine();
+            Console.WriteLine("Which set of files would you like to compare? 1,2 or 3 "); //Asking the user which set of files they would like to compare
+            string choice = Console.ReadLine(); //The user's choice saved as a varible
 
-            if (choice == "1")
+            if (choice == "1") 
             {
-                if (git_1a == git_1b)
+                if (git_1a == git_1b) //Comparison between the two chosen files
                 {
-                    Console.WriteLine("They are the same");
+                    Console.WriteLine("They are the same"); //One of the results of the comparison
                 }
                 else
                 {
-                    Console.WriteLine("They are not the same");
+                    Console.WriteLine("They are not the same"); //One of the results of the comparison
                 }
             }
 
-            if (choice == "2")
+            if (choice == "2") //Comparison between the two chosen files
             {
                 if (git_2a == git_2b)
                 {
-                    Console.WriteLine("They are the same");
+                    Console.WriteLine("They are the same"); //One of the results of the comparison
                 }
                 else
                 {
-                    Console.WriteLine("They are not the same");
+                    Console.WriteLine("They are not the same"); //One of the results of the comparison
                 }
             }
 
-            if (choice == "3")
+            if (choice == "3") //Comparison between the two chosen files
             {
                 if (git_3a == git_3b)
                 {
-                    Console.WriteLine("They are the same");
+                    Console.WriteLine("They are the same"); //One of the results of the comparison
                 }
                 else
                 {
-                    Console.WriteLine("They are not the same");
+                    Console.WriteLine("They are not the same"); //One of the results of the comparison
                 }
             }
         }
